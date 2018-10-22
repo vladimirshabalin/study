8dabad56412b7c3bbe7927b9988f9d34e35d932a
Vladimir Shabalin <vladimir.shabalin@anychart.com>
2018-10-23 01:11:29 +0800

    php hw1

---
+++ php1/ls1/index.php
@@ -0,0 +1,19 @@
+<?php
+$title = 'Document';
+$headline = 'Гость, добро пожаловать к нам на сайт!';
+$date = 'Сегодя 19.10.2018';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $title; ?></title>
+</head>
+<body>
+    <h1><?php echo $headline; ?></h1>
+    <p><?php echo $date; ?></p>
+<!--Можно вывести через 1 echo, но тогда в html коде не будет нормальных отступов.
+    Собственно такой вариант мне нравится больше, но как правильно я не знаю, надеюсь на уроке расскаже.-->
+<?php //echo "<h1>$headline</h1> <p>$date</p>"; ?>
+</body>
+</html>
